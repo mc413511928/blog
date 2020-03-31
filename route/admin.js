b7c9f4d44b8faffc275e6967cf31f7741c71b193
ifer <iamajser@163.com>
2020-03-31 11:30:52 +0800

    模板文件优化

--- route/admin.js
+++ route/admin.js
@@ -8,4 +8,9 @@ admin.get('/login', (req, res) => {
     res.render('admin/login', {});
 });
 
+admin.get('/user', (req, res) => {
+    // 渲染 admin 模板
+    res.render('admin/user', {});
+});
+
 module.exports = admin;
