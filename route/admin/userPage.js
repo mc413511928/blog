d2f24458e0d477a1b75a6b5c81c2dd67852b84ea
ifer <iamajser@163.com>
2020-04-03 10:25:03 +0800

    展示用户列表数据

--- route/admin/userPage.js
+++ route/admin/userPage.js
@@ -2,10 +2,9 @@ const { User } = require('../../model/user');
 
 module.exports = async (req, res) => {
     const users = await User.find();
-    res.send(users);
     // 渲染 admin 模板
-    // res.render('admin/user', {
-    //     userInfo: req.session.userInfo,
-    //     users
-    // });
+    res.render('admin/user', {
+        userInfo: req.session.userInfo,
+        users
+    });
 };
