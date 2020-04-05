cfb6dc8f5a461fbd1b7d29fcc68c884fa5b93d2b
ifer <iamajser@163.com>
2020-04-05 15:51:56 +0800

    文章列表分页处理mongoose-sex-page

--- route/admin/articlePage.js
+++ route/admin/articlePage.js
@@ -1,9 +1,16 @@
 const { Article } = require('../../model/article');
+const pagination = require('mongoose-sex-page');
 
 module.exports = async (req, res) => {
     req.app.locals.currentLink = 'article';
+    let page = req.query.page || 1;
+    // page: 当前页
+    // size: 没页显示多少条
+    // display: 需要展示多少页
+    // exec: 执行查询操作
+    let articles = await pagination(Article).find().page(page).size(3).display(5).populate('author').exec();
 
-    let articles = await Article.find().populate('author');
+    // res.send(articles);
 
     // res.send(articles);
     res.render('admin/article', {
