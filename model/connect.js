2ebc9c4f0f64abb8bf26a7a757a2543be3c9f607
ifer <iamajser@163.com>
2020-04-05 16:43:20 +0800

    为MongoDB添加权限

--- model/connect.js
+++ model/connect.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/blog', {
+mongoose.connect('mongodb://ifer:ifer@localhost:27017/blog', {
     useNewUrlParser: true,
     useUnifiedTopology: true
 }).then(() => console.log('=============数据库连接成功============')).catch(err => console.log(err, '数据库连接失败'));
