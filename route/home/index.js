621080ff9e3251a8a5787b38f7ad7207ec6076b0
ifer <iamajser@163.com>
2020-04-06 14:24:49 +0800

    前台文章列表展示

--- route/home/index.js
+++ route/home/index.js
@@ -1,3 +1,15 @@
-module.exports = (req, res) => {
-    res.render('home/default')
+const { Article } = require('../../model/article');
+const pagination = require('mongoose-sex-page');
+
+module.exports = async (req, res) => {
+    let page = req.query.page || 1;
+    // page: 哪一页
+    // size: 一页显示多少条
+    // display: 显示多少页
+    // exec: 执行前面的查询操作
+    const articles = await pagination(Article).find().page(page).size(10).display(5).populate('author').exec();
+    // res.send(articles);
+    res.render('home/default', {
+        articles
+    });
 };
