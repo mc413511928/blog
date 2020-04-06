4ad01817378a2f2ceb962b71022ef27c8fa51e12
ifer <iamajser@163.com>
2020-04-06 10:37:24 +0800

    使用config自动区分环境并提取配置信息

--- model/connect.js
+++ model/connect.js
@@ -1,6 +1,9 @@
 const mongoose = require('mongoose');
+const config = require('config');
 
-mongoose.connect('mongodb://ifer:ifer@localhost:27017/blog', {
+// mongodb://ifer:ifer@localhost:27017/blog
+
+mongoose.connect(`mongodb://${config.get('db.user')}:${config.get('db.pwd')}@${config.get('db.host')}:${config.get('db.port')}/${config.get('db.name')}`, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 }).then(() => console.log('=============数据库连接成功============')).catch(err => console.log(err, '数据库连接失败'));
