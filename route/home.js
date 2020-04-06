bbded1bc656cbbf6c45f73ff0053affa8530fc9f
ifer <iamajser@163.com>
2020-04-06 11:29:56 +0800

    前台模板的展示和优化

--- route/home.js
+++ route/home.js
@@ -2,9 +2,8 @@ const express = require('express');
 
 // 创建路由对象
 const home = express.Router();
-
-home.get('/', (req, res) => {
-    res.send('前台首页');
-});
+// 为什么没有自动找 home 下的 index.js 呢？
+home.get('/', require('./home/index'));
+home.get('/article', require('./home/article'));
 
 module.exports = home;
