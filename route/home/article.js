0aefdd822b6a0184c877c3b464fd111483c0f3a6
ifer <iamajser@163.com>
2020-04-07 09:37:46 +0800

    评论列表

--- route/home/article.js
+++ route/home/article.js
@@ -1,15 +1,21 @@
 const {
     Article
 } = require('../../model/article');
+const Comment = require('../../model/comment');
 
 module.exports = async (req, res) => {
+    // 文章ID
     let _id = req.query.id;
     let article = await Article.findOne({
         _id
     }).populate('author');
-    // res.send(article);
+
+    // 查询当前文章下的所有评论
+    const commets = await Comment.find({aid: _id}).populate('uid');
+    // res.send(commets);
     res.render('home/article', {
         article,
-        userInfo: req.session.userInfo
+        userInfo: req.session.userInfo,
+        commets
     });
 };
